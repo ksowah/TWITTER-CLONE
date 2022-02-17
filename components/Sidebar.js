fe3c2c9fca16c398f3a9a8413be472684e76e51a
ksowah <sowahkelvin640@gmail.com>
2022-02-17 14:40:36 +0000

    update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,12 @@
-
+import Image from 'next/image'
 const Sidebar = () => {
   return (
-    <div className="hidden">
-        
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+        <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+            <Image 
+                src={'https://rb.gy/ogau5a'}
+            />
+        </div>
     </div>
   )
 }
