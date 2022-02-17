f2f10c6549aeb874e257e9e526dcdbf0b30f162d
ksowah <sowahkelvin640@gmail.com>
2022-02-17 21:57:35 +0000

    update

--- components/Login.js
+++ components/Login.js
@@ -14,15 +14,15 @@ const Login = ({providers}) => {
         <div className="flex flex-col border border-gray-300 h-64 w-72 p-4 items-center justify-center">
             {Object.values(providers).map((provider) => (
                 <>
-              <div  
+              <button key={provider.name} 
+                    onClick={()=>signIn(provider.id, { callbackUrl: '/' })} 
                     className='flex p-2 px-6 bg-sky-500 hover:bg-sky-600  text-white text-center mt-8 cursor-pointer'>
                     <h1 className="flex items-center justify-between font-medium">
                         Login with Google <FcGoogle className="ml-2 text-xl"/>
                     </h1>
-                </div>
+                </button>
                 
-                <button key={provider.name} 
-                    onClick={()=>signIn(provider.id, { callbackUrl: '/' })}
+                <button 
                     class="cursor-pointer relative mt-8 inline-flex items-center justify-start px-6 p-2 overflow-hidden font-medium transition-all bg-black hover:bg-white group border">
                     <span class="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
                     <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black flex items-center">Login With Github <AiFillGithub className="ml-2 text-xl"/></span>
