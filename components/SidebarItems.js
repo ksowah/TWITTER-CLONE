a286602b830cd24c0f15419fe501c5b72f398be7
ksowah <sowahkelvin640@gmail.com>
2022-02-17 15:17:58 +0000

    update

--- components/SidebarItems.js
+++ components/SidebarItems.js
@@ -1,7 +1,7 @@
 
 const SidebarItems = ({Icon, text, active}) => {
   return (
-    <div className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation`}>
+    <div className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation ${active && 'font-bold'}`}>
         <Icon className='h-7 text-white'/>
         <span className="hidden xl:inline ">{text}</span>
     </div>
