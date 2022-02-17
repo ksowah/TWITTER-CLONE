09fb4fa23660658ac29d3c6f344af6e12809a4ee
ksowah <sowahkelvin640@gmail.com>
2022-02-17 16:22:45 +0000

    update

--- components/Input.js
+++ components/Input.js
@@ -1,14 +1,23 @@
+import { useState } from 'react'
 
 const Input = () => {
+
+    const [input, setInput] = useState()
+
   return (
-    <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}>
+    <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-auto`}>
         <img src="https://avatars.githubusercontent.com/u/80095257?v=4"
             className="h-11 w-11 rounded-full cursor-pointer"
         />
 
         <div className="w-full divide-y divide-gray-700">
-            <div>
-                <textarea />
+            <div className={``}>
+                <textarea value={input} rows='2'
+                onChange={(e)=>setInput(e.target.value)}
+                 placeholder='What are you currently working on?' 
+                className='bg-transparent outline-none 
+                text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full' 
+                />
             </div>
         </div>
     </div>
