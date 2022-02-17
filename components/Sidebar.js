481ed6ba4be7786bb255390debc004e8835741e3
ksowah <sowahkelvin640@gmail.com>
2022-02-17 14:43:28 +0000

    update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,6 +5,8 @@ const Sidebar = () => {
         <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
             <Image 
                 src={'https://rb.gy/ogau5a'}
+                width={30}
+                height={30}
             />
         </div>
     </div>
