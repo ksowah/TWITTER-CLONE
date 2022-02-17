a8242290be965df15e7ec6705431de8149614162
ksowah <sowahkelvin640@gmail.com>
2022-02-17 21:39:47 +0000

    update

--- components/Login.js
+++ components/Login.js
@@ -19,12 +19,7 @@ const Login = () => {
                     Login with Google <FcGoogle className="ml-2 text-xl"/>
                 </h1>
             </div>
-            {/* <div  
-                className='flex p-2 border border-gray-300 px-6 bg-black text-white text-center mt-8 cursor-pointer'>
-                <h1 className="flex items-center justify-between font-medium">
-                    Login with Github <AiFillGithub className="ml-2 text-xl"/>
-                </h1>
-            </div> */}
+           
             <a href="#_" class="relative mt-8 inline-flex items-center justify-start px-6 p-2 overflow-hidden font-medium transition-all bg-black hover:bg-white group border">
                 <span class="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
                 <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black flex items-center">Login With Github <AiFillGithub className="ml-2 text-xl"/></span>
