23fbc71ba75333bb9431e110e5d58926401b086d
ksowah <sowahkelvin640@gmail.com>
2022-02-17 19:51:20 +0000

    update

--- components/Input.js
+++ components/Input.js
@@ -29,8 +29,14 @@ const Input = () => {
         setInput(input + emoji)
     }
 
-    const addImageToPost = () => {
-
+    const addImageToPost = (e) => {
+        const reader = new FileReader()
+        if(e.target.files[0]){
+            reader.readAsDataURL(e.target.files[0])
+        }
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result)
+        }
     }
 
     const sendPostToDB = async () => {
