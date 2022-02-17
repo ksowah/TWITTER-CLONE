27421fe0e9fc4991d39f044053fa967edbe86ca2
ksowah <sowahkelvin640@gmail.com>
2022-02-17 14:57:18 +0000

    update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import SidebarItems from '../components/SidebarItems'
+
 const Sidebar = () => {
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
