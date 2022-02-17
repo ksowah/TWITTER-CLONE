5c2547d297f973fee6a1de9ca2ee6984334843a8
ksowah <sowahkelvin640@gmail.com>
2022-02-17 16:12:49 +0000

    update

--- components/Input.js
+++ components/Input.js
@@ -1,10 +1,16 @@
 
 const Input = () => {
   return (
-    <div>
+    <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}>
         <img src="https://avatars.githubusercontent.com/u/80095257?v=4"
             className="h-11 w-11 rounded-full cursor-pointer"
         />
+
+        <div className="w-full divide-y divide-gray-700">
+            <div>
+                <textarea />
+            </div>
+        </div>
     </div>
   )
 }
