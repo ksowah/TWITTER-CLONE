aa417c771ee8480750cd2aaba8aa130ad62c0649
ksowah <sowahkelvin640@gmail.com>
2022-02-18 21:35:37 +0000

    update

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Post = ({id, post}) => {
+const Post = ({id, post, postPage}) => {
   return (
-    <div>
-        
+    <div className='p-3 flex cursor-pointer border-b border-gray-700'>
+        {!postPage && (
+            <img src={post?.userProfile} 
+            className="h-11 w-11 rounded-full mr-4"/>
+        )}
+
+        <div className='flex flex-col space-y-2 w-4'>
+
+        </div>
     </div>
   )
 }
