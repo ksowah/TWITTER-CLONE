c8313d00256fd4bb2bd7f5604f59385ba4045332
ksowah <sowahkelvin640@gmail.com>
2022-02-18 21:40:21 +0000

    update

--- components/Post.js
+++ components/Post.js
@@ -8,8 +8,16 @@ const Post = ({id, post, postPage}) => {
             className="h-11 w-11 rounded-full mr-4"/>
         )}
 
-        <div className='flex flex-col space-y-2 w-4'>
-
+        <div className='flex flex-col space-y-2 w-full'>
+            <div className={`flex ${!postPage && "justify-between"}`}>
+                {postPage  && (
+                    <img 
+                        src={post?.userProfile}
+                        alt="profile Pic"
+                        className="h-11 w-11 roumded-full mr-4"
+                    />
+                )}
+            </div>
         </div>
     </div>
   )
