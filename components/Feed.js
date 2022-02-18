504b2bb6a02a3b7f2902db02724e92f596c79eba
ksowah <sowahkelvin640@gmail.com>
2022-02-18 21:12:36 +0000

    update

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,11 @@
 import {SparklesIcon} from '@heroicons/react/outline'
+import { useState } from 'react'
 import Input from '../components/Input'
 
 const Feed = () => {
+
+  const [post, setPost] = useState([])
+
   return (
     <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
         <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -12,6 +16,9 @@ const Feed = () => {
             
         </div>
         <Input />
+        <div className='pb-72'>
+            
+        </div>
     </div>
   )
 }
