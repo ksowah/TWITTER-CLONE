88aaa421ff1ded6804ba4e4692c5ed1c937d023e
ksowah <sowahkelvin640@gmail.com>
2022-02-18 19:46:10 +0000

    update

--- components/Login.js
+++ components/Login.js
@@ -12,7 +12,7 @@ const Login = ({providers}) => {
 
         <div className='h-screen flex flex-col items-center justify-center'>
         <div className="flex flex-col border border-gray-300 h-64 w-72 p-4 items-center justify-center">
-            {Object.values(providers).map((provider) => (
+            {Object.values(providers && providers).map((provider) => (
                 <>
               <button key={provider.name} 
                     onClick={()=>signIn(provider.id, { callbackUrl: '/' })} 
@@ -22,11 +22,11 @@ const Login = ({providers}) => {
                     </h1>
                 </button>
                 
-                <button 
+                {/* <button 
                     class="cursor-pointer relative mt-8 inline-flex items-center justify-start px-6 p-2 overflow-hidden font-medium transition-all bg-black hover:bg-white group border">
                     <span class="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
                     <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black flex items-center">Login With Github <AiFillGithub className="ml-2 text-xl"/></span>
-                </button>
+                </button> */}
                 </>
             
             ))}
