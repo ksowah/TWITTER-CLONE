9613badbfcc3189aa0c7925a2a6045674138bbce
ksowah <sowahkelvin640@gmail.com>
2022-02-19 12:25:15 +0000

    update

--- components/Modal.js
+++ components/Modal.js
@@ -1,8 +1,27 @@
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
 
 const Modal = () => {
-  return (
-    <div>Modal</div>
-  )
+    let [isOpen, setIsOpen] = useState(true)
+
+    return (
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        <Dialog.Overlay />
+  
+        <Dialog.Title>Deactivate account</Dialog.Title>
+        <Dialog.Description>
+          This will permanently deactivate your account
+        </Dialog.Description>
+  
+        <p>
+          Are you sure you want to deactivate your account? All of your data will
+          be permanently removed. This action cannot be undone.
+        </p>
+  
+        <button onClick={() => setIsOpen(false)}>Deactivate</button>
+        <button onClick={() => setIsOpen(false)}>Cancel</button>
+      </Dialog>
+    )
 }
 
 export default Modal
