8795c3ad170de00fab08a39dbfc4550bf98be2b0
ksowah <sowahkelvin640@gmail.com>
2022-02-19 15:05:38 +0000

    update

--- components/Modal.js
+++ components/Modal.js
@@ -2,7 +2,7 @@ import { Dialog, Transition } from '@headlessui/react'
 import { CalendarIcon, ChartBarIcon, EmojiHappyIcon, PhotographIcon } from '@heroicons/react/outline'
 import { XIcon } from '@heroicons/react/solid'
 import { Picker } from 'emoji-mart'
-import { addDoc, doc, onSnapshot, serverTimestamp } from 'firebase/firestore'
+import { addDoc, collection, doc, onSnapshot, serverTimestamp } from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { Fragment, useEffect, useState } from 'react'
@@ -132,12 +132,12 @@ const sendComment = async (e) => {
                             </div>     
                         </div>
                         <div className='mt-7 flex space-x-3 w-full'>
-                             <img src={session.user.image} className='h-11 w-11 rounded-full'/>
+                             <img src={session.user.image} className='h-8 w-8 ml-[6px] rounded-full'/>
                             <div className='flex-grow mt-2'>
                                 <textarea 
                                     value={comment}
                                     onChange={(e)=> setComment(e.target.value)}
-                                    placeholder={`send ${post?.username} a feedback`}
+                                    placeholder={`Send ${post?.username} a feedback`}
                                     rows='2'
                                     className='bg-transparent outline-none text-[#d9d9d9] text-md placeholder-gray-500 tracking-wide w-full min-h-[80px]'
                                 />
@@ -178,7 +178,8 @@ const sendComment = async (e) => {
 
             <button className='bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md
             hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default'
-            >
+             onClick={sendComment}
+           >
                 Send
             </button>
         </div>
