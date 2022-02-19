da54143ca04b72475427e991c96217dcf3c28f34
ksowah <sowahkelvin640@gmail.com>
2022-02-19 01:20:14 +0000

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import { RecoilRoot } from "recoil"
 
 function MyApp({ Component, 
   pageProps: { session, ...pageProps }, }) {
   return (
     <SessionProvider session={session}>
+      <RecoilRoot>
         <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   )}
 
