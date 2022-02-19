93064a04140ac0c4c9f1515586947526b6fc5bb8
ksowah <sowahkelvin640@gmail.com>
2022-02-19 23:03:11 +0000

    update

--- components/Comment.js
+++ components/Comment.js
@@ -1,8 +1,10 @@
 
 
-const Comment = () => {
+const Comment = ({id, comment}) => {
   return (
-    <div>Comment</div>
+    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+        <img src={comment?.userProfile} className="h-11 w-11 rounded-full mr-4"/>
+    </div>
   )
 }
 
