3cfc9649661e8f1e9526b1571da618998e553fc9
ksowah <sowahkelvin640@gmail.com>
2022-02-19 21:41:20 +0000

    update

--- pages/[id].js
+++ pages/[id].js
@@ -1,8 +1,19 @@
 function PostPage (){
     return(
-    <div>
-
-    </div>
+    <div className=''>
+        <Head>
+          <title>The Dashboard</title>
+          <meta name="description" content="Developers can publish their projects"/>
+          <link rel="icon" href="https://th.bing.com/th/id/R.6f6f59ca9a56444cfff8d89aaa09d100?rik=pAkm1nprRZgYVA&pid=ImgRaw&r=0" />
+        </Head>
+  
+        <main className='flex bg-black min-h-screen max-w-[1500px] mx-auto'>
+          <Sidebar />
+          <Feed />
+          {/* Widget */}
+            {isOpen && <Modal />} 
+        </main>
+      </div>
     )
 }
 
