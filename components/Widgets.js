0864e59e0ee427738411df2290724de588c174f2
ksowah <sowahkelvin640@gmail.com>
2022-02-20 19:57:21 +0000

    update

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,7 +1,12 @@
+import { SearchIcon } from "@heroicons/react/outline"
 
 const Widgets = () => {
   return (
-    <div>Widgets</div>
+    <div className="hidden lg:inine ml-8 xl:w-[450px] py-1 space-y-5">
+        <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
+            <SearchIcon className="text-gray-500 h-5 z-50"/>
+        </div>
+    </div>
   )
 }
 
