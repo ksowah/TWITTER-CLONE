a09340d445bc22c70e870fab009b01ecbcc9076d
ksowah <sowahkelvin640@gmail.com>
2022-02-20 20:19:26 +0000

    update

--- components/Widgets.js
+++ components/Widgets.js
@@ -6,10 +6,20 @@ const Widgets = ({followResults, trendingResults}) => {
         <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
             <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
             <SearchIcon className="text-gray-500 h-5 z-50"/>
-            <input type="text" className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full"/>
-            
+            <input type="text" className="bg-transparent placeholder-gray-500 outline-none 
+                text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full"/>
+
             </div>
-           </div>
+        </div>
+        <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
+            <h4 className="font-bold text-xl px-4">
+                What's happening
+            </h4>
+
+            {trendingResults.map((result, index) => (
+                <Trending key={index} result={result} />
+            ))}
+        </div>
     </div>
   )
 }
