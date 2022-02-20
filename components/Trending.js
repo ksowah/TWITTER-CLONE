c071d81d0d98b0c7327e3900c8f8ddc3c6393e7a
ksowah <sowahkelvin640@gmail.com>
2022-02-20 20:45:09 +0000

    update

--- components/Trending.js
+++ components/Trending.js
@@ -1,3 +1,4 @@
+import { DotsHorizontalIcon } from "@heroicons/react/solid"
 
 const Trending = ({result}) => {
   return (
@@ -11,7 +12,7 @@ const Trending = ({result}) => {
             <h6 className="font-bold max-w-[250px] text-sm">
                 {result?.description}
             </h6>
-            <p className="">
+            <p className="text-[#6e767d] text-xs font-medium max-w-[250px]">
                 Trending with {result.tags.map((tag, index) => (
                     <span key={index} className='tag'>
                         {tag}
@@ -19,6 +20,19 @@ const Trending = ({result}) => {
                 ))}
             </p>
         </div>
+        {result.img ? (
+            <Image 
+                src={result.imd}
+                width={70}
+                height={70}
+                objectFit="cover"
+                className="rounded-2xl"
+            />
+        ) : (
+            <div className="icon group">
+                <DotsHorizontalIcon className="h-5 text-[#6e767d] group-hover:text-[#1d9bf0]"/>
+            </div>
+        )}
     </div>
   )
 }
