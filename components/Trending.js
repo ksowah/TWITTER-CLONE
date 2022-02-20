e7a0535bcb384790cd5c3d02c076252ac769777d
ksowah <sowahkelvin640@gmail.com>
2022-02-20 20:28:00 +0000

    update

--- components/Trending.js
+++ components/Trending.js
@@ -1,7 +1,11 @@
 
-const Trending = () => {
+const Trending = ({results}) => {
   return (
-    <div>Trending</div>
+    <div className="hover:bg-white hover:bg-opacity-[0.03]
+    px-4 py-3 cursor-pointer transition duration-200 ease-out flex 
+    items-center justify-between">
+        
+    </div>
   )
 }
 
