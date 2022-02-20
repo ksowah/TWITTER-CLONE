9658bd016d0d0602215244cd9abb319333a7c556
ksowah <sowahkelvin640@gmail.com>
2022-02-20 20:33:37 +0000

    update

--- components/Trending.js
+++ components/Trending.js
@@ -1,10 +1,16 @@
 
-const Trending = ({results}) => {
+const Trending = ({result}) => {
   return (
     <div className="hover:bg-white hover:bg-opacity-[0.03]
     px-4 py-3 cursor-pointer transition duration-200 ease-out flex 
     items-center justify-between">
-        
+        <div className="space-y-0.5">
+            <p className="text-[#6e767d] text-xs font-medium">
+                {result?.heading}
+            </p>
+            <h6 className="font-bold max-w-[250px] text-sm">
+                {result?.description}</h6>
+        </div>
     </div>
   )
 }
