2789e76e91ce540e84f68f88d70c29ba4034c87b
ksowah <sowahkelvin640@gmail.com>
2022-02-20 20:02:00 +0000

    update

--- components/Widgets.js
+++ components/Widgets.js
@@ -5,6 +5,8 @@ const Widgets = () => {
     <div className="hidden lg:inine ml-8 xl:w-[450px] py-1 space-y-5">
         <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
             <SearchIcon className="text-gray-500 h-5 z-50"/>
+            <input type="text" className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full"/>
+            yoooooo
         </div>
     </div>
   )
