2f9866609ca7c0cadb65db464993fda6108b66a9
ksowah <sowahkelvin640@gmail.com>
2022-02-20 21:36:37 +0000

    update

--- components/Widgets.js
+++ components/Widgets.js
@@ -4,11 +4,11 @@ import Trending from "./Trending"
 
 const Widgets = ({followResults, trendingResults}) => {
   return (
-    <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
+    <div className="hidden lg:inline ml-8 xl:w-[750px] py-1 space-y-5">
         <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
             <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
             <SearchIcon className="text-gray-500 h-5 z-50"/>
-            <input type="text" className="bg-transparent placeholder-gray-500 outline-none 
+            <input type="text" className="bg-transparent placeholder-gray-500 outline-none focus:border-[#1d9bf0] rounded-full focus:bg-black
                 text-[#d9d9d9] absolute inset-0 pl-11 border border-transparent w-full"/>
 
             </div>
