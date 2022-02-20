dc8dddd6ca869ac32d85dc8044855eab77b9d3cf
ksowah <sowahkelvin640@gmail.com>
2022-02-20 20:04:37 +0000

    update

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,6 @@
 import { SearchIcon } from "@heroicons/react/outline"
 
-const Widgets = () => {
+const Widgets = ({followResults, trendingResults}) => {
   return (
     <div className="hidden lg:inine ml-8 xl:w-[450px] py-1 space-y-5">
         <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
