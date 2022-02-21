919af2e0c30c56c595688e86bcb173a27d9ef037
ksowah <sowahkelvin640@gmail.com>
2022-02-21 19:13:40 +0000

    add to widgets

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,4 +1,5 @@
 import { SearchIcon } from "@heroicons/react/outline"
+import Image from "next/image"
 import Trending from "./Trending"
 
 const Widgets = ({followResults, trendingResults}) => {
@@ -19,6 +20,37 @@ const Widgets = ({followResults, trendingResults}) => {
 
             {trendingResults?.map((result, index) => (
                 <Trending key={index} result={result} />
+            ))}
+
+            <button className="hover:bg-white hover:bg-opacity-[0.03]
+             px-4 py-3 cursor-pointer transition duration-200 ease-out flex 
+             items-center justify-between w-full text-[#1d9bf0] font-light">
+                 Show more
+            </button>
+        </div>
+
+        <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
+            <h4 className="font-bold text-xl px-4">
+               Who to follow
+            </h4>
+
+            {followResults.map((result, index) => (
+                <div key={index} className='hover:bg-white hover:bg-opacity-[0.03]
+                px-4 py-3 cursor-pointer transition duration-200 ease-out flex 
+                items-center'>
+                    <Image 
+                        src={result.userImg}
+                        width={50}
+                        height={50}
+                        objectFit="cover"
+                        className="rounded-full"
+                    />
+                    <div className="ml-4 leading-5 group">
+                        <h4 className="font-bold group-hover:underline">{result.username}</h4>
+                        <h5 className="text-gray-500 text-[15px]">{result.tag}</h5>
+                    </div>
+                    <button className="ml-auto bg-white text-black rounded-full font-bold text-sm py-1.5 px-3.5">Follow</button>
+                </div>
             ))}
 
             <button className="hover:bg-white hover:bg-opacity-[0.03]
