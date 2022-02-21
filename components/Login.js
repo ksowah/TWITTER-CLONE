4eb04318c09ada6c1e20e0ccd1dc98de4ca239fc
ksowah <sowahkelvin640@gmail.com>
2022-02-21 13:26:29 +0000

    update

--- components/Login.js
+++ components/Login.js
@@ -23,7 +23,7 @@ const Login = ({providers}) => {
                     onClick={()=>signIn(provider.id, { callbackUrl: '/' })} 
                     className='cursor-pointer relative mt-12 inline-flex items-center justify-start px-6 p-2 overflow-hidden font-medium transition-all bg-black hover:bg-white group border'>
                         <span className="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                        <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black group-hover:font-bold flex items-center">Sign in With Google <FcGoogle className="ml-2 text-xl"/></span>
+                        <span className="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black group-hover:font-bold flex items-center">Sign in With Google <FcGoogle className="ml-2 text-xl"/></span>
                    
                 </button>
                 
