df4da6fa1bc06b039d5c9bd7919382020588a659
ksowah <sowahkelvin640@gmail.com>
2022-02-21 19:06:35 +0000

    resize width of feed

--- components/Feed.js
+++ components/Feed.js
@@ -18,7 +18,7 @@ const Feed = () => {
     ), [db])
 
   return (
-    <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+    <div className="text-white flex-grow border-l border-r border-gray-700 max-w-xl sm:ml-[73px] xl:ml-[370px]">
         <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
             <h2 className='text-lg sm:xl font-bold'>Home</h2>
             <div className='hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto'>
--- pages/[id].js
+++ pages/[id].js
@@ -47,7 +47,7 @@ function PostPage ({providers, trendingResults, followResults}){
   
         <main className='flex bg-black min-h-screen max-w-[1500px] mx-auto'>
           <Sidebar />
-          <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+          <div className="flex-grow border-l border-r border-gray-700 max-w-xl sm:ml-[73px] xl:ml-[370px]">
             <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
                 <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0">
                      <ArrowLeftIcon className="h-5 text-white" onClick={() => router.push("/")}/>
