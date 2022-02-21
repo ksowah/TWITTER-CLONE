67c9fcc0fc6b5d0256cbe19d6e19c8fec77e8286
ksowah <sowahkelvin640@gmail.com>
2022-02-21 12:24:21 +0000

    update

--- components/Login.js
+++ components/Login.js
@@ -21,9 +21,10 @@ const Login = ({providers}) => {
                 <>
               <button key={provider.name} 
                     onClick={()=>signIn(provider.id, { callbackUrl: '/' })} 
-                    className='cursor-pointer relative mt-8 inline-flex items-center justify-start px-6 p-2 overflow-hidden font-medium transition-all bg-black hover:bg-sky-500 group border'>
-                        <span className="w-48 h-48 rounded rotate-[-40deg] bg-sky-500 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                        <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black flex items-center">Sign in With Google <FcGoogle className="ml-2 text-xl"/></span>
+                    className='cursor-pointer relative mt-12 inline-flex items-center justify-start px-6 p-2 overflow-hidden font-medium transition-all bg-black hover:bg-white group border'>
+                        <span className="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                        <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black group-hover:font-bold flex items-center">Sign in With Google <FcGoogle className="ml-2 text-xl"/></span>
+                   
                 </button>
                 
                 </>
@@ -36,11 +37,3 @@ const Login = ({providers}) => {
 }
 
 export default Login
-
-/**
- *  <button 
-                    class="cursor-pointer relative mt-8 inline-flex items-center justify-start px-6 p-2 overflow-hidden font-medium transition-all bg-black hover:bg-white group border">
-                    <span class="w-48 h-48 rounded rotate-[-40deg] bg-white absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                    <span class="relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-black flex items-center">Login With Github <AiFillGithub className="ml-2 text-xl"/></span>
-                </button>
- */
